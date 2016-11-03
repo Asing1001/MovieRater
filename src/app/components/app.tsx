a6bc2f1fef065568bc8d439d2b9f6f9a955bd311
AndyChen <shinningstar1001@gmail.com>
2016-11-04 03:39:24 +0800

    add search autocomplete logics

--- src/app/components/app.tsx
+++ src/app/components/app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
+import 'isomorphic-fetch';
 
 injectTapEventPlugin();
 
--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -1,22 +1,47 @@
 import * as React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import AutoComplete from 'material-ui/AutoComplete';
+import { YahooMovie } from '../../crawler/yahooCrawler';
+
 
 class Home extends React.Component<any, any> {
+  allMovies: Array<YahooMovie> = [];
+  allMoviesName: Array<String> = [];
   constructor(props) {
     super(props)
     this.state = {
       dataSource: [],
     };
+    fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: "{allMovies{chineseTitle,englishTitle}}" }),
+      credentials: 'include',
+    }).then(res => res.json())
+      .then(json => {
+        this.allMovies = json.data.allMovies;
+        json.data.allMovies.forEach(({chineseTitle, englishTitle}: YahooMovie) => {
+          return this.allMoviesName.push(chineseTitle, englishTitle)
+        });
+      });
   }
 
-  handleUpdateInput = (value) => {
+  handleUpdateInput = (value:string) => {
+    let searchWord = value.toLowerCase();
+    let matchMovies = [];
+    this.allMoviesName.every(movieName => {
+      if (movieName.toLowerCase().indexOf(searchWord) !== -1) {
+        matchMovies.push(movieName)
+      }
+
+      return matchMovies.length !== 6;
+    });
+
     this.setState({
-      dataSource: [
-        value,
-        value + value,
-        value + value + value,
-      ],
+      dataSource: matchMovies,
     });
   };
 
@@ -29,12 +54,12 @@ class Home extends React.Component<any, any> {
     return (
       <div>
         <AutoComplete
-          hintText="Type anything"
+          hintText="電影名稱(中英皆可)"
           dataSource={this.state.dataSource}
           onUpdateInput={this.handleUpdateInput}
-          floatingLabelText="Full width"
+          floatingLabelText="找電影"
           fullWidth={true}
-        />
+          />
       </div>
     );
   }
