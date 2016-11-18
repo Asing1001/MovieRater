e55af8f65d240c0891138542cd11979f819ad952
AsinChen <shinningstar1001@gmail.com>
2016-11-18 19:18:18 +0800

    revert dburl

--- src/configs/systemSetting.ts
+++ src/configs/systemSetting.ts
@@ -1,7 +1,7 @@
 export const systemSetting = {
-    // dbUrl : 'mongodb://acmLab1001:6RsEeqp9FfKJ@ds145415.mlab.com:45415/movierater',
+    dbUrl : 'mongodb://acmLab1001:6RsEeqp9FfKJ@ds145415.mlab.com:45415/movierater',
     websiteUrl: 'http://movieqat.azurewebsites.net/',
-    dbUrl : 'mongodb://localhost:27017/movierater'
+    // dbUrl : 'mongodb://localhost:27017/movierater'
 }
 
 export const pttCrawlerSetting = {
