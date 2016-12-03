c21077d41edee1186242efb8e61c080f9c4f84fb
Andy Chen <shinningstar1001@gmail.com>
2016-12-04 00:46:57 +0800

    add toggle functions too pttarticle

--- src/app/components/pttArticles.tsx
+++ src/app/components/pttArticles.tsx
@@ -1,16 +1,58 @@
 import * as React from 'react';
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
 import Movie from '../../models/movie';
+import Article from '../../models/article';
 import { List, ListItem } from 'material-ui/List';
 import { Checkbox } from 'material-ui/Checkbox';
-
+import Toggle from 'material-ui/Toggle';
 interface MovieDetailProps {
     movie: Movie
 }
 
-class PttArticles extends React.Component<MovieDetailProps, null> {
+enum ArticleType {
+    GoodRate, badRate, normalRate, other
+}
+
+class PttArticles extends React.Component<MovieDetailProps, any> {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            filteredArticles: [],
+            goodRateToggle: true,
+            badRateToggle: true,
+            normalRateToggle: true,
+            otherToggle: true
+        }
+    }
+
+    componentWillReceiveProps(nextProps: MovieDetailProps) {
+        if (this.props !== nextProps) {
+            this.getFilterdArticles();
+        }
+    }
+
+    private toggleGoodRate(){
+        this.setState({goodRateToggle:!this.state.goodRateToggle})
+        this.getFilterdArticles();
+    }
+
+    private handleToggle(articleType : ArticleType){
+        // switch(articleType){
+        //     case ArticleType.GoodRate:
+        //         this.setState({goodRateToggle:!this.state.goodRateToggle})
+        // }
+        // alert(articleType)
+        //this.getFilterdArticles();
+        return null;
+    }
+
+    private getFilterdArticles() {
+        let movie = this.props.movie;
+        let filteredArticles = [].concat(this.state.goodRateToggle ? movie.goodRateArticles : [],
+            this.state.badRateToggle ? movie.badRateArticles : [],
+            this.state.normalRateToggle ? movie.normalRateArticles : [],
+            this.state.otherToggle ? movie.otherArticles : [])
+        this.setState({ filteredArticles: filteredArticles });
     }
 
     private getPttPushColor(push: string) {
@@ -29,11 +71,16 @@ class PttArticles extends React.Component<MovieDetailProps, null> {
 
     render() {
         return (
+
             <div className="col-xs-12" style={{ background: 'black', height: '100%' }}>
-                {this.props.movie.relatedArticles.length === 0 ?
+                <Toggle
+                    label="GoodRate" style={{ color: 'white'}} toggled={this.state.goodRateToggle} onToggle={this.toggleGoodRate.bind(this)}
+                    />
+                {this.state.goodRateToggle}
+                {this.state.filteredArticles.length === 0 ?
                     <h4 style={{ color: '#aaa', textAlign: "center" }}>找不到相關文章</h4> :
                     <List>
-                        {this.props.movie.relatedArticles.map(article => {
+                        {this.state.filteredArticles.map(article => {
                             return <ListItem
                                 innerDivStyle={{ paddingLeft: '56px', background: 'black', cursor: 'initial' }}
                                 key={article.url}
