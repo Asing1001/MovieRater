ae4e0c2b7ab90437f171ba561be22b005d255125
Andy Chen <shinningstar1001@gmail.com>
2016-12-10 22:28:00 +0800

    add summary and adjust findresult

--- src/app/components/findResult.tsx
+++ src/app/components/findResult.tsx
@@ -15,20 +15,24 @@ class FindResult extends React.Component<MovieDetailProps, {}> {
     render() {
         return (
             <Paper zDepth={2} className="row no-margin" style={{ marginTop: '.5em' }}>
-                <div className="col-md-2 hidden-xs no-padding">
-                    <img src={this.props.movie.posterUrl} alt="" className="img-responsive" />
+                <div className="col-xs-2 no-padding">
+                    <img src={this.props.movie.posterUrl} alt="" className="img-responsive" style={{minWidth:'60px'}}/>
                 </div>
-                <div className="col-xs-12 col-md-10" style={{ paddingBottom: '1em' }}>
-                    <div style={{paddingTop:'.5em',paddingBottom:'.5em'}}>
-                    <b>{this.props.movie.chineseTitle}({this.props.movie.englishTitle})</b>
-                    <div className="row">
-                        <span className="col-xs-7 col-md-3">上映日: {this.props.movie.releaseDate}</span>
-                        <span className="hidden-xs col-md-3">類型: {this.props.movie.type}</span>
-                        <span className="col-xs-5 col-md-3 no-padding">片長: {this.props.movie.runTime}</span>
-                    </div>
+                <div className="col-xs-10" style={{ paddingBottom: '.5em' }}>
+                    <div style={{ paddingTop: '.5em', paddingBottom: '.5em' }}>
+                        <b>{this.props.movie.chineseTitle}({this.props.movie.englishTitle})</b>
+                        <div>
+                            <span style={{ paddingRight: '1em' }}>上映日: {this.props.movie.releaseDate}</span>
+                            <span className="hidden-xs" style={{ paddingRight: '1em' }}>類型: {this.props.movie.type}</span>
+                            <span>片長: {this.props.movie.runTime}</span>
+                        </div>
                     </div>
                     <Ratings movie={this.props.movie}></Ratings>
-                     </div>
+                    <div className="hidden-xs">
+                        <div className="resultSummary" dangerouslySetInnerHTML={{ __html: this.props.movie.summary }}></div>
+                        <a href="">continue reading...</a>
+                    </div>
+                </div>
             </Paper>
         );
     };
