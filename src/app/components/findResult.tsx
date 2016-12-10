8522158ca20267d4d67db5079be8d8b8804477a1
Andy Chen <shinningstar1001@gmail.com>
2016-12-10 19:42:14 +0800

    modify css for result listing

--- src/app/components/findResult.tsx
+++ src/app/components/findResult.tsx
@@ -19,9 +19,16 @@ class FindResult extends React.Component<MovieDetailProps, {}> {
                     <img src={this.props.movie.posterUrl} alt="" className="img-responsive" />
                 </div>
                 <div className="col-xs-12 col-md-10" style={{ paddingBottom: '1em' }}>
-                    <h5>{this.props.movie.chineseTitle}({this.props.movie.englishTitle})</h5>
+                    <div style={{paddingTop:'.5em',paddingBottom:'.5em'}}>
+                    <b>{this.props.movie.chineseTitle}({this.props.movie.englishTitle})</b>
+                    <div className="row">
+                        <span className="col-xs-7 col-md-3">上映日: {this.props.movie.releaseDate}</span>
+                        <span className="hidden-xs col-md-3">類型: {this.props.movie.type}</span>
+                        <span className="col-xs-5 col-md-3 no-padding">片長: {this.props.movie.runTime}</span>
+                    </div>
+                    </div>
                     <Ratings movie={this.props.movie}></Ratings>
-                </div>
+                     </div>
             </Paper>
         );
     };
