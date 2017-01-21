d9a1e88a7388a6469b9666a1b1497bd1f8fef9b0
AsinChen <shinningstar1001@gmail.com>
2017-01-21 05:30:53 -0600

    separate db from qat

--- src/configs/systemSetting.ts
+++ src/configs/systemSetting.ts
@@ -1,8 +1,8 @@
 const isProduction = process.env.ENV === 'production';
 
 export const systemSetting = {
-    dbUrl: 'mongodb://acmLab1001:6RsEeqp9FfKJ@ds145415.mlab.com:45415/movierater?connectTimeoutMS=300000',
-    websiteUrls: ['http://movieqat.azurewebsites.net/', 'http://movierater.azurewebsites.net/'],
+    dbUrl: 'mongodb://acmLab1001:MtOURxGiU2SL@ds117109.mlab.com:17109/movieraterprd',
+    websiteUrls: ['http://movierater.azurewebsites.net/'],
     //dbUrl : 'mongodb://localhost:27017/movierater'
 }
 
