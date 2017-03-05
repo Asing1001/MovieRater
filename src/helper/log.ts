878a8c0fc92dccd507eb9dd5033a2c7873bd8d22
AndyChen <shinningstar1001@gmail.com>
2017-03-06 03:39:47 +0800

    improve log

--- src/helper/log.ts
+++ src/helper/log.ts
@@ -1,7 +1,8 @@
 let log = {
     debug: function (args) {
-        let functionName = new Error().stack.split(' ')[11];
-        console.log(functionName, args);
+        let functionName = new Error().stack.split(' ')[11].substr(9);
+        let logArgs = Array.from(args).map(arg => JSON.stringify(arg).substr(0, 100)).join(', ');
+        console.log(`${new Date()} : ${functionName}(${logArgs})`);
     }
 }
 
