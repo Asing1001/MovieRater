9fe7bc6231d3cc0d8fc807f0701aab8e1e311ed4
AndyChen <shinningstar1001@gmail.com>
2017-03-06 03:56:29 +0800

    use momentjs to format date in log.ts

--- src/helper/log.ts
+++ src/helper/log.ts
@@ -1,8 +1,10 @@
+import * as moment from 'moment';
+
 let log = {
     debug: function (args) {
         let functionName = new Error().stack.split(' ')[11].substr(9);
         let logArgs = Array.from(args).map(arg => JSON.stringify(arg).substr(0, 100)).join(', ');
-        console.log(`${new Date()} : ${functionName}(${logArgs})`);
+        console.log(`${moment().utcOffset(8).format()}: ${functionName}(${logArgs})`);
     }
 }
 
