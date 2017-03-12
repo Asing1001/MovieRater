fc252fad70b7ff9eac91f38301c7536db21bd04d
AndyChen <shinningstar1001@gmail.com>
2017-03-12 12:13:47 +0000

    enhance performance by gzip and disable pretty print (https://app.codeship.com/projects/182204/builds/23358787) --skip-ci

--- dist/configs/systemSetting.js
+++ dist/configs/systemSetting.js
@@ -4,6 +4,7 @@ exports.systemSetting = {
     dbUrl: process.env.DB_URL || 'mongodb://acmLab1001:6RsEeqp9FfKJ@ds145415.mlab.com:45415/movierater',
     //dbUrl : 'mongodb://localhost:27017/movierater'
     websiteUrl: process.env.WEBSITE_URL,
+    enableGraphiql: process.env.ENABLE_GRAPHIQL
 };
 exports.pttCrawlerSetting = {
     enable: false,
