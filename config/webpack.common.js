a7c78c53a52aaa9a8e1b0a79b0f37ff43c134fbc
Max Chou <max9159@gmail.com>
2017-04-16 19:30:04 +0800

    Add manifest.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -20,6 +20,10 @@ module.exports = {
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
                 loader: 'file?name=public/[name].[hash].[ext]'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader',
             }
         ],
         preLoaders: [
