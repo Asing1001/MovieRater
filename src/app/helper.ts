6b2a905f12da1b9ecc9f967e1b822adade72e34d
max9159 <max9159@gmail.com>
2017-04-26 00:34:28 +0800

    Add get client geo location and calculate function --skip-ci

--- src/app/helper.ts
+++ src/app/helper.ts
@@ -23,7 +23,33 @@ export function classifyArticle(movie: Movie) {
 }
 
 export function requestGraphQL(query: string) {
-    return fetch(`/graphql?query=${query.replace(/\s+/g,"")}`).then(res => {
+    return fetch(`/graphql?query=${query.replace(/\s+/g, "")}`).then(res => {
         return res.json()
     })
+}
+
+export function getClientGeoLocation() {
+    return new Promise((reslove, reject) => window.navigator.geolocation.getCurrentPosition(
+        function (pos) {
+            reslove(pos);
+            var lat = pos.coords.latitude;
+            var lon = pos.coords.longitude;
+        }, reject
+    ));
+}
+
+export function distanceInKM(lon1, lat1, lon2, lat2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = getRadians(lat2 - lat1);  // Javascript functions in radians
+    var dLon = getRadians(lon2 - lon1);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(getRadians(lat1)) * Math.cos(getRadians(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function getRadians(num) {
+    return (num) * Math.PI / 180;
 }
