1020ff0b8322f2988424bc2205ef45e1e2bb3335
Unknown <max9159@gmail.com>
2017-04-29 10:45:38 +0000

    Merged branch AddNavigationBar into master (https://app.codeship.com/projects/182204/builds/24746037) --skip-ci

--- dist/app/components/home.js
+++ dist/app/components/home.js
@@ -4,9 +4,15 @@ const React = require("react");
 const AutoComplete_1 = require("material-ui/AutoComplete");
 require("isomorphic-fetch");
 const react_router_1 = require("react-router");
+const Drawer_1 = require("material-ui/Drawer");
+const movie_1 = require("material-ui/svg-icons/av/movie");
+const AppBar_1 = require("material-ui/AppBar");
+const List_1 = require("material-ui/List");
 class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.handleToggle = () => this.setState({ open: !this.state.open });
+        this.handleClose = () => { this.setState({ open: false }); react_router_1.browserHistory.push(`/`); };
         this.state = {
             searchText: '',
             dataSource: [],
@@ -50,11 +56,13 @@ class Home extends React.Component {
         }
     }
     render() {
-        return (React.createElement("div", { className: "container" },
-            React.createElement("div", { className: "autoCompleteWrapper" },
-                React.createElement(AutoComplete_1.default, { hintText: "電影名稱(中英皆可)", dataSource: this.state.dataSource, floatingLabelText: "找電影", fullWidth: true, filter: AutoComplete_1.default.caseInsensitiveFilter, maxSearchResults: 6, onNewRequest: this.onNewRequest.bind(this), searchText: this.state.searchText, onUpdateInput: this.handleUpdateInput.bind(this) }),
-                React.createElement("button", { className: `clearButton ${this.state.searchText ? '' : 'displayNone'}`, onClick: this.clearSearchText.bind(this) }, "X")),
-            this.props.children));
+        return (React.createElement("div", null,
+            React.createElement(AppBar_1.default, { onLeftIconButtonTouchTap: this.handleToggle, iconStyleLeft: { marginTop: "0px" }, className: "appBar", style: { height: "48px" }, children: React.createElement(AutoComplete_1.default, { hintText: React.createElement("span", null, "\u641C\u5C0B\u96FB\u5F71\u540D\u7A31(\u4E2D\u82F1\u7686\u53EF)"), dataSource: this.state.dataSource, fullWidth: true, filter: AutoComplete_1.default.caseInsensitiveFilter, maxSearchResults: 6, onNewRequest: this.onNewRequest.bind(this), searchText: this.state.searchText, onUpdateInput: this.handleUpdateInput.bind(this), menuStyle: { minWidth: '500px' } }) }),
+            React.createElement("div", { className: "container", style: { marginTop: '.5em' } },
+                this.props.children,
+                React.createElement(Drawer_1.default, { docked: false, width: 300, containerStyle: { maxWidth: '75%' }, open: this.state.open, onRequestChange: (open) => this.setState({ open }) },
+                    React.createElement(List_1.List, null,
+                        React.createElement(List_1.ListItem, { onTouchTap: this.handleClose, leftIcon: React.createElement(movie_1.default, null) }, "\u73FE\u6B63\u4E0A\u6620"))))));
     }
 }
 exports.default = Home;
