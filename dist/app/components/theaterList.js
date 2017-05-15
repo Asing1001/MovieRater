68cc6c1d8d2e81db2ef2df010b8932a9d7968639
AndyChen <shinningstar1001@gmail.com>
2017-05-15 18:18:32 +0000

    add theater distance (https://app.codeship.com/projects/182204/builds/25159190) --skip-ci

--- dist/app/components/theaterList.js
+++ dist/app/components/theaterList.js
@@ -36,37 +36,34 @@ const defaultSubRegion = '全部地區';
 class TheaterList extends React.Component {
     constructor(props) {
         super(props);
+        this.getTheatersWithDistance = () => {
+            helper_2.getClientGeoLocation().then(({ latitude, longitude }) => {
+                const theatersWithDistance = this.state.theaters.map((theater) => {
+                    const { location: { lat, lng } } = theater;
+                    return Object.assign({ distance: helper_2.getDistanceInKM(lng, lat, longitude, latitude) }, theater);
+                }).sort(({ distance: distanceA }, { distance: distanceB }) => distanceA - distanceB);
+                this.setState({ theaters: theatersWithDistance });
+            });
+        };
         this.handleChange = (event, index, selectedSubRegion) => this.setState({ selectedSubRegion });
         this.state = {
             theaters: [],
-            isLoading: true,
             subRegions: [],
+            isLoading: true,
             selectedSubRegion: defaultSubRegion
         };
     }
     getData(ids) {
-        helper_1.requestGraphQL(`${theaterQuery}`).then((json) => {
-            this.setState({
+        return helper_1.requestGraphQL(`${theaterQuery}`).then((json) => {
+            return this.setState({
                 theaters: json.data.theaters,
                 subRegions: [...new Set(json.data.theaters.map(({ subRegion }) => subRegion))],
                 isLoading: false
             });
         });
     }
-    componentWillReceiveProps(nextProps) {
-        this.getData(nextProps.params.ids);
-    }
     componentDidMount() {
-        this.getData(this.props.params.ids);
-    }
-    gettheatersWithDistance() {
-        helper_2.getClientGeoLocation().then(({ latitude, longitude }) => {
-            const theatersWithDistance = this.props.schedules.map((schedule) => {
-                const { theaterExtension: { location: { lat, lng } } } = schedule;
-                return Object.assign({ distance: helper_2.getDistanceInKM(lng, lat, longitude, latitude) }, schedule);
-            }).sort(({ distance: distanceA }, { distance: distanceB }) => distanceA - distanceB);
-            this.setState({ theatersWithDistance });
-        });
+        this.getData(this.props.params.ids).then(this.getTheatersWithDistance);
     }
     render() {
         return (React.createElement("div", null,
@@ -74,9 +71,9 @@ class TheaterList extends React.Component {
             React.createElement("div", { className: "col-xs-12" },
                 React.createElement(SelectField_1.default, { value: this.state.selectedSubRegion, onChange: this.handleChange },
                     React.createElement(MenuItem_1.default, { value: defaultSubRegion, primaryText: defaultSubRegion }),
-                    this.state.subRegions.map(subRegion => React.createElement(MenuItem_1.default, { value: subRegion, primaryText: subRegion })))),
+                    this.state.subRegions.map((subRegion, index) => React.createElement(MenuItem_1.default, { key: index, value: subRegion, primaryText: subRegion })))),
             this.state.theaters.filter(({ subRegion }) => this.state.selectedSubRegion === defaultSubRegion || subRegion === this.state.selectedSubRegion)
-                .map(({ name, address, phone, }) => (React.createElement(Paper_1.default, { zDepth: 1, className: "col-xs-12", style: { paddingBottom: '.5em' } },
+                .map(({ name, address, phone, distance }, index) => (React.createElement(Paper_1.default, { zDepth: 2, className: "col-xs-12", style: { paddingBottom: '.5em' }, key: index },
                 React.createElement("div", { style: { paddingTop: '.5em', paddingBottom: '.5em' } },
                     React.createElement(react_router_1.Link, { style: { color: 'inherit' }, to: `/theater/${name}` },
                         React.createElement("h5", { style: { marginBottom: "-.2em", fontSize: "16px" } }, name)),
@@ -87,7 +84,8 @@ class TheaterList extends React.Component {
                         React.createElement("a", { href: `https://maps.google.com?q=${name}`, style: theaterInfoStyle },
                             React.createElement(location_on_1.default, { color: colors_1.grey500, viewBox: '-3 0 30 24' }),
                             address,
-                            " (x.x km)"))))))));
+                            " ",
+                            distance && ` (${distance} km)`))))))));
     }
 }
 exports.default = TheaterList;
