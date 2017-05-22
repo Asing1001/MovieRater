6b2dd1b47c702205264635f7a156254f0aba3225
Andy Chen <shinningstar1001@gmail.com>
2017-05-22 08:38:21 +0000

    Revert systemsetting change (https://app.codeship.com/projects/182204/builds/25340312) --skip-ci

--- dist/configs/systemSetting.js
+++ dist/configs/systemSetting.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.systemSetting = {
-    //dbUrl: process.env.DB_URL || 'mongodb://acmLab1001:6RsEeqp9FfKJ@ds145415.mlab.com:45415/movierater',
-    dbUrl: 'mongodb://localhost:27017/movierater',
+    dbUrl: process.env.DB_URL || 'mongodb://acmLab1001:6RsEeqp9FfKJ@ds145415.mlab.com:45415/movierater',
+    //dbUrl : 'mongodb://localhost:27017/movierater'
     websiteUrl: process.env.WEBSITE_URL,
-    enableGraphiql: process.env.ENABLE_GRAPHIQL || true,
-    enableScheduler: false
+    enableGraphiql: process.env.ENABLE_GRAPHIQL,
+    enableScheduler: true
 };
 exports.schedulerSetting = {
     pttPagePerTime: 50,
