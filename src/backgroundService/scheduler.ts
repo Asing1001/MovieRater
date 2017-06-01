ca940404114a0c069a289117dbd0b899dab31c6d
AsinChen <shinningstar1001@gmail.com>
2017-06-02 00:58:58 +0800

    use cronjob to seperate memory loading

--- src/backgroundService/scheduler.ts
+++ src/backgroundService/scheduler.ts
@@ -1,47 +1,52 @@
+import * as fetch from "isomorphic-fetch";
+import { scheduleJob } from 'node-schedule';
+import { systemSetting, schedulerSetting } from '../configs/systemSetting';
 import { updateYahooMovies, updateTheaterWithLocationList } from '../task/yahooTask';
 import { updatePttArticles } from '../task/pttTask';
-import { systemSetting, schedulerSetting } from '../configs/systemSetting';
-import * as fetch from "isomorphic-fetch";
-import cacheManager from '../data/cacheManager';
 import { updateImdbInfo } from '../task/imdbTask';
+import cacheManager from '../data/cacheManager';
 
 export function initScheduler() {
     if (!systemSetting.enableScheduler) {
         return;
     }
-    console.log("[initScheduler]");
+    console.log("[Scheduler] init");
     setInterval(function () {
         fetch(systemSetting.websiteUrl).then(res =>
             console.log(`[Scheduler] Access to website:${systemSetting.websiteUrl}, status:${res.status}`));
     }, 600000);
 
-    setInterval(async function () {
-        console.time('[Scheduler] crawlYahoo');
+    scheduleJob('10 * * * *', async function () {
+        console.time('[Scheduler] updateYahooMovies');
         await updateYahooMovies(schedulerSetting.yahooPagePerTime)
-        console.timeEnd('[Scheduler] crawlYahoo');
-    }, 3600000);
+        console.timeEnd('[Scheduler] updateYahooMovies');
+    });
 
-    setInterval(async function () {
+    scheduleJob('15 * * * *', async function () {
+        console.time('[Scheduler] updatePttArticles');
+        await updatePttArticles(schedulerSetting.pttPagePerTime);
+        console.timeEnd('[Scheduler] updatePttArticles');
+    });
+
+    scheduleJob('20 * * * *', function () {
+        cacheManager.setInTheaterMoviesCache();
+    });
+
+    scheduleJob('30 5 * * *', async function () {
+        console.time('[Scheduler] updateTheaterWithLocationList');
+        await updateTheaterWithLocationList();
+        console.timeEnd('[Scheduler] updateTheaterWithLocationList');
+    });
+
+    scheduleJob('40 5 * * *', async function () {
+        console.time('[Scheduler] cacheManager.init');
+        await cacheManager.init();
+        console.timeEnd('[Scheduler] cacheManager.init');
+    });
+
+    scheduleJob('30 6 * * *', async function () {
         console.time('[Scheduler] updateImdbInfo');
         await updateImdbInfo()
         console.timeEnd('[Scheduler] updateImdbInfo');
-    }, 3600000 * 12);
-
-    setInterval(async function () {
-        console.time('[Scheduler] crawlPtt');
-        await updatePttArticles(schedulerSetting.pttPagePerTime);
-        console.timeEnd('[Scheduler] crawlPtt');
-    }, 3600000);
-
-    setInterval(function () {
-        cacheManager.init();
-    }, 86400000);
-
-    setInterval(function () {
-        cacheManager.setInTheaterMoviesCache();
-    }, 3600000);
-
-    setInterval(function () {
-        updateTheaterWithLocationList();
-    }, 86400000);
+    });
 }
