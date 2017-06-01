cd05e79e591ef2764c3a49b8712e583f190c519d
AsinChen <shinningstar1001@gmail.com>
2017-06-01 19:58:51 +0000

    add status code to movie not found (https://app.codeship.com/projects/182204/builds/25661465) --skip-ci

--- dist/app/components/movieNotFound.js
+++ dist/app/components/movieNotFound.js
@@ -1,23 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
+const status_1 = require("./status");
 class MovieNotFound extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
-    }
-    componentWillReceiveProps(nextProps) {
-        // this.getData(nextProps.match.params.ids);
     }
     render() {
-        return (React.createElement("div", { style: {
-                display: 'flex',
-                alignItems: 'center',
-                textAlign: 'center',
-                justifyContent: 'center',
-                height: '150px',
-            } },
-            React.createElement("h4", null, `資料庫還沒有${this.props.match.params.query}的相關資料唷！`)));
+        return (React.createElement(status_1.default, { code: 404 },
+            React.createElement("div", { style: {
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                    justifyContent: 'center',
+                    height: '150px',
+                } },
+                React.createElement("h4", null, `資料庫還沒有${this.props.match.params.query}的相關資料唷！`))));
     }
 }
 exports.default = MovieNotFound;
