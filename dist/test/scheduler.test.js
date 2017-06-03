1fd52bb7b3896f97dfe29f1315da23237714f190
AndyChen <shinningstar1001@gmail.com>
2017-06-03 07:50:46 +0000

    improve scheduler testing (https://app.codeship.com/projects/182204/builds/25703895) --skip-ci

--- dist/test/scheduler.test.js
+++ dist/test/scheduler.test.js
@@ -1,20 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised");
-const db_1 = require("../data/db");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
 const scheduler_1 = require("../backgroundService/scheduler");
+const systemSetting_1 = require("../configs/systemSetting");
+const nodeSchedule = require("node-schedule");
+chai.use(sinonChai);
 const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
-chai.should();
-chai.use(chaiAsPromised);
 describe('Scheduler', () => {
+    let consoleSpy, scheduleJobSpy;
+    beforeEach(() => {
+        consoleSpy = sinon.spy(console, 'log');
+        scheduleJobSpy = sinon.spy(nodeSchedule, 'scheduleJob');
+    });
+    afterEach(() => {
+        consoleSpy.restore();
+        scheduleJobSpy.restore();
+    });
     describe('initScheduler', () => {
-        before(() => { return db_1.db.openDbConnection(); });
-        it('should init schedules without exception', function () {
-            this.timeout(30000);
-            return scheduler_1.initScheduler();
+        it('should log "[Scheduler] init" if enableScheduler=true', function () {
+            systemSetting_1.systemSetting.enableScheduler = true;
+            scheduler_1.initScheduler();
+            consoleSpy.calledWithExactly("[Scheduler] init").should.be.true;
+            scheduleJobSpy.called.should.be.true;
+        });
+        it('console.log should not be call if enableScheduler=false', function () {
+            systemSetting_1.systemSetting.enableScheduler = false;
+            scheduler_1.initScheduler();
+            consoleSpy.notCalled.should.be.true;
+            scheduleJobSpy.notCalled.should.be.true;
         });
     });
 });
