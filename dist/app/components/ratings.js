dc935ef3c6e9da3e702c5d8498ca6dfa4fcb0224
Andy Chen <shinningstar1001@gmail.com>
2018-02-04 15:31:53 +0000

    Merge branch \'master\' into Production (https://app.codeship.com/projects/182204/builds/31898714) --skip-ci

--- dist/app/components/ratings.js
+++ dist/app/components/ratings.js
@@ -8,18 +8,18 @@ class Ratings extends React.Component {
     render() {
         return (React.createElement("div", { className: this.props.className, style: this.props.style },
             React.createElement("div", { className: "ratingWrapper" },
-                React.createElement("img", { src: "/public/image/imdb.png" }),
+                React.createElement("span", { className: "imdb logo" }, "IMDb"),
                 this.props.movie.imdbID ? React.createElement("a", { href: "http://www.imdb.com/title/" + this.props.movie.imdbID }, this.props.movie.imdbRating ? this.props.movie.imdbRating : 'N/A')
                     : React.createElement("span", null, this.props.movie.imdbRating ? this.props.movie.imdbRating : 'N/A')),
             React.createElement("div", { className: "ratingWrapper" },
-                React.createElement("img", { src: "/public/image/yahoo.png" }),
+                React.createElement("span", { className: "yahoo logo" }, "Y!"),
                 React.createElement("a", { href: "https://movies.yahoo.com.tw/movieinfo_main.html/id=" + this.props.movie.yahooId }, parseInt(this.props.movie.yahooRating) ? this.props.movie.yahooRating : 'N/A')),
             React.createElement("div", { className: "ratingWrapper hide" },
                 React.createElement("img", { src: "/public/image/rottentomatoes.png" }),
                 this.props.movie.tomatoURL && this.props.movie.tomatoURL !== 'N/A' ? React.createElement("a", { href: this.props.movie.tomatoURL }, this.props.movie.tomatoRating ? this.props.movie.tomatoRating : 'N/A')
                     : React.createElement("span", null, this.props.movie.tomatoRating ? this.props.movie.tomatoRating : 'N/A')),
             React.createElement("div", { className: "ratingWrapper" },
-                React.createElement("span", { className: "pttLogo" }, "PTT"),
+                React.createElement("span", { className: "ptt logo" }, "PTT"),
                 React.createElement("span", { className: "hint--bottom", "aria-label": "(好雷/普雷/負雷)" },
                     this.props.movie.goodRateArticles.length,
                     "/",
