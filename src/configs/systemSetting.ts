2a23717d99fa3ec743bfd67220a9575f25b743cf
Sing <sing.chen@linecorp.com>
2020-10-11 01:45:09 +0800

    Migrate Mlab to Atlas mongoDB

--- src/configs/systemSetting.ts
+++ src/configs/systemSetting.ts
@@ -1,5 +1,5 @@
 export const systemSetting = {
-    dbUrl: process.env.DB_URL || 'mongodb://acmLab1001:6RsEeqp9FfKJ@ds145415.mlab.com:45415/movierater',
+    dbUrl: process.env.DB_URL || 'mongodb://acmLab1001:6RsEeqp9FfKJ@movierater-shard-00-00.t36sx.mongodb.net:27017,movierater-shard-00-01.t36sx.mongodb.net:27017,movierater-shard-00-02.t36sx.mongodb.net:27017/movierater?ssl=true&replicaSet=atlas-aqn4du-shard-0&authSource=admin&retryWrites=true&w=majority',
     // dbUrl : 'mongodb://localhost:27017/movierater',
     websiteUrl: process.env.WEBSITE_URL,
     enableGraphiql: process.env.ENABLE_GRAPHIQL === 'true',
