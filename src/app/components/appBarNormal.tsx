b91c28686e057808aeecda9874e99a005630081b
Sing <sing.chen@linecorp.com>
2023-05-10 23:08:55 +0800

    Remove sort button when there is nothing to sort

--- src/app/components/appBarNormal.tsx
+++ src/app/components/appBarNormal.tsx
@@ -9,47 +9,121 @@ import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import { SortType } from '../sorting';
 import muiThemeable from 'material-ui/styles/muiThemeable';
+import { Route, Switch } from 'react-router-dom';
 
 const normalStyles = {
-    color: 'white'
-}
+  color: 'white',
+};
 
+const routesWithSort = [
+  { path: '/', exact: true },
+  { path: 'upcoming' },
+  { path: '/movies/:ids' },
+  { path: '/theater/:name' },
+];
 class AppBarNormal extends React.PureComponent<any, any> {
-    constructor(props) {
-        super(props)
-        this.state = {
-            barTitle: '現正上映'
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      barTitle: '現正上映',
+    };
+  }
 
-    drawerToggle = null;
+  drawerToggle = null;
 
-    render() {
-        return (
-            <Paper zDepth={2} className={`appBar normal ${this.props.className}`} style={{color: normalStyles.color, backgroundColor: this.props.muiTheme.palette.primary1Color }}>
-                <IconButton className="leftBtn" onTouchTap={e => { e.preventDefault(); this.drawerToggle() }}><SVGNavigationMenu color={normalStyles.color} /></IconButton>
-                <span className="barTitle">{this.state.barTitle}</span>
-                <span onClick={this.props.onSearchIconClick} className="hidden-xs searchArea" style={{ backgroundColor: this.props.muiTheme.palette.primary2Color }}>
-                    <SVGActionSearch className="searchIcon" color={normalStyles.color} />
-                    <span>搜尋電影名稱(中英皆可)</span>
-                </span>
-                <IconButton onTouchTap={e => { e.preventDefault(); this.props.onSearchIconClick() }} className="visible-xs rightBtn"><SVGActionSearch color={normalStyles.color} /></IconButton>
+  render() {
+    return (
+      <Paper
+        zDepth={2}
+        className={`appBar normal ${this.props.className}`}
+        style={{
+          color: normalStyles.color,
+          backgroundColor: this.props.muiTheme.palette.primary1Color,
+        }}
+      >
+        <IconButton
+          className="leftBtn"
+          onTouchTap={(e) => {
+            e.preventDefault();
+            this.drawerToggle();
+          }}
+        >
+          <SVGNavigationMenu color={normalStyles.color} />
+        </IconButton>
+        <span className="barTitle">{this.state.barTitle}</span>
+        <span
+          onClick={this.props.onSearchIconClick}
+          className="hidden-xs searchArea"
+          style={{ backgroundColor: this.props.muiTheme.palette.primary2Color }}
+        >
+          <SVGActionSearch className="searchIcon" color={normalStyles.color} />
+          <span>搜尋電影名稱(中英皆可)</span>
+        </span>
+        <IconButton
+          onTouchTap={(e) => {
+            e.preventDefault();
+            this.props.onSearchIconClick();
+          }}
+          className="visible-xs rightBtn"
+        >
+          <SVGActionSearch color={normalStyles.color} />
+        </IconButton>
+        <Switch>
+          {routesWithSort.map((route, index) => (
+            <Route
+              key={index}
+              {...route}
+              render={(routeProps) => (
                 <IconMenu
-                    className="sortingBtn"
-                    iconButtonElement={
-                        <IconButton><SVGContentSort color={normalStyles.color} /></IconButton>
-                    }
-                    targetOrigin={{ horizontal: 'right', vertical: 'top' }}
-                    anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  className="sortingBtn"
+                  iconButtonElement={
+                    <IconButton>
+                      <SVGContentSort color={normalStyles.color} />
+                    </IconButton>
+                  }
+                  targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                  anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
                 >
-                    <MenuItem primaryText="依IMDB排序" onTouchTap={e => { e.preventDefault(); this.props.switchSorting(SortType.imdb) }} />
-                    <MenuItem primaryText="依Yahoo排序" onTouchTap={e => { e.preventDefault(); this.props.switchSorting(SortType.yahoo) }} />
-                    <MenuItem primaryText="依Ptt排序" onTouchTap={e => { e.preventDefault(); this.props.switchSorting(SortType.ptt) }} />
-                    <MenuItem primaryText="依上映日期排序" onTouchTap={e => { e.preventDefault(); this.props.switchSorting(SortType.releaseDate) }} />
+                  <MenuItem
+                    primaryText="依IMDB排序"
+                    onTouchTap={(e) => {
+                      e.preventDefault();
+                      this.props.switchSorting(SortType.imdb);
+                    }}
+                  />
+                  <MenuItem
+                    primaryText="依Yahoo排序"
+                    onTouchTap={(e) => {
+                      e.preventDefault();
+                      this.props.switchSorting(SortType.yahoo);
+                    }}
+                  />
+                  <MenuItem
+                    primaryText="依Ptt排序"
+                    onTouchTap={(e) => {
+                      e.preventDefault();
+                      this.props.switchSorting(SortType.ptt);
+                    }}
+                  />
+                  <MenuItem
+                    primaryText="依上映日期排序"
+                    onTouchTap={(e) => {
+                      e.preventDefault();
+                      this.props.switchSorting(SortType.releaseDate);
+                    }}
+                  />
                 </IconMenu>
-                <AppDrawer changeTitle={barTitle => this.setState({ barTitle })} ref={instance => this.drawerToggle = instance && instance.toggle} />
-            </Paper>
-        );
-    }
+              )}
+            />
+          ))}
+        </Switch>
+
+        <AppDrawer
+          changeTitle={(barTitle) => this.setState({ barTitle })}
+          ref={(instance) => (this.drawerToggle = instance && instance.toggle)}
+        />
+      </Paper>
+    );
+  }
 }
 export default muiThemeable()(AppBarNormal);
