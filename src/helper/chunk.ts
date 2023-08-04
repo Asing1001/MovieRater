9c88f0d2772b1b986ba8d2c85d490d6ca73e2074
Sing <sing.chen@linecorp.com>
2023-08-04 21:42:28 +0800

    ✨ Add chunk helper

---
+++ src/helper/chunk.ts
@@ -0,0 +1,7 @@
+export function chunk(items: any[], size: number) {
+  const chunks = [];
+  for (let i = 0; i < items.length; i += size) {
+    chunks.push(items.slice(i, i + size));
+  }
+  return chunks;
+}
