a8ebcf652d50199dd5e930a57009f035693d54ef
Sing Ming Chen <sing.chen@bytedance.com>
2023-10-30 22:16:57 +0000

    continue when no targetId found

--- src/data/findMoviesBy.ts
+++ src/data/findMoviesBy.ts
@@ -1,13 +1,18 @@
-import Movie from "../models/movie";
+import Movie from '../models/movie';
 
-export function findMoviesBy(allMovies: Movie[], ids: [string], findBy: "movieBaseId" | "yahooId") {
+export function findMoviesBy(allMovies: Movie[], ids: [string], findBy: 'movieBaseId' | 'yahooId') {
   const idSet = new Set(ids);
   const movies = [];
 
   for (const movie of allMovies) {
-    if (idSet.has(movie[findBy].toString())) {
+    const targetId = movie[findBy];
+    if (!targetId) {
+      continue;
+    }
+
+    if (idSet.has(targetId.toString())) {
       movies.push(movie);
-      idSet.delete(movie[findBy].toString());
+      idSet.delete(targetId.toString());
     }
 
     if (idSet.size === 0) {
@@ -16,5 +21,4 @@ export function findMoviesBy(allMovies: Movie[], ids: [string], findBy: "movieBa
   }
 
   return movies;
-
 }
