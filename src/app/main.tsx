52d57e3556a2b19bea60fcc45a8f96d8678f6025
Sing Ming Chen <sing.chen@bytedance.com>
2023-11-28 00:22:43 +0000

    remove virtual pageview because GA could track it automatically

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,20 +4,16 @@ import * as ReactDOM from 'react-dom';
 import App from './components/app';
 import createBrowserHistory from 'history/createBrowserHistory';
 import './main.scss';
-import {
-  ApolloClient,
-  ApolloProvider,
-  createNetworkInterface
-} from 'react-apollo';
+import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
 
 class Root extends React.PureComponent<any, any> {
   createClient() {
     return new ApolloClient({
-      initialState: window["__APOLLO_STATE__"] || {},
+      initialState: window['__APOLLO_STATE__'] || {},
       ssrForceFetchDelay: 100,
       networkInterface: createNetworkInterface({
-        uri: '/graphql'
-      })
+        uri: '/graphql',
+      }),
     });
   }
 
@@ -33,18 +29,6 @@ class Root extends React.PureComponent<any, any> {
 }
 
 const history = createBrowserHistory();
-history.listen((location, action) => {
-  const dataLayer = window['dataLayer'];
-  if (dataLayer) {
-    dataLayer.push({
-      'event': 'virtualPageView',
-      'page': {
-        'url': location.pathname
-      }
-    });
-  }
-})
-
 const rootElement = document.getElementById('app');
 ReactDOM.render(<Root></Root>, rootElement);
 
