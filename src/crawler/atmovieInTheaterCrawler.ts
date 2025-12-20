7a8ed7b21ef679c88e7fb3b768dd9bb066d57cf1
Sing <sing@paddingleft.com>
2025-12-20 10:17:49 +0000

    ✨ Trim whitespace from movie names in getInTheaterMovieNames function

--- src/crawler/atmovieInTheaterCrawler.ts
+++ src/crawler/atmovieInTheaterCrawler.ts
@@ -5,10 +5,11 @@ export async function getInTheaterMovieNames() {
     let movieName = [];
     try {
         const $ = await getCheerio$(inTheaterUrl);
-        movieName = Array.from($('.filmListPA>li>a')).map(a=>$(a).text());
+        movieName = Array.from($('.filmListPA>li>a')).map(a=>$(a).text().trim());
         if(!movieName.length) {
             console.warn('getInTheaterMovieNames got 0 movie, the class may have been changed!')
         }
+        console.log(movieName)
     } catch (error) {
         console.error('getInTheaterMovieNames fail!!')
         console.error(error);
